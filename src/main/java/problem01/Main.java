544b97020897c8d2fafd1bae53993257eff78640
bit <bit@121.138.83.68>
2019-02-14 10:20:26 +0900

    1,4번 완료

--- src/main/java/problem01/Main.java
+++ src/main/java/problem01/Main.java
@@ -21,6 +21,14 @@ public class Main {
 	}
 	
 	public static int countClap(int number) {
-		return 0;
+		int cnt=0;
+		String num_str = Integer.toString(number);
+		int size = num_str.length();
+		for(int i=0; i<size; i++) {
+			if((num_str.charAt(i) == '3') || (num_str.charAt(i) == '6') || (num_str.charAt(i) == '9')) {
+				cnt++;
+			}
+		}
+		return cnt;
 	}
 }
--- src/main/java/problem04/Main.java
+++ src/main/java/problem04/Main.java
@@ -1,7 +1,41 @@
 package problem04;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-
+		Scanner scan = new Scanner(System.in);
+				
+		//int random_num = (int)(Math.random() * 1000) +1;
+		int random_num = 783;
+		System.out.println(random_num);
+		int N = 0; //도전 횟수
+		
+		String ran_str = Integer.toString(random_num);
+		
+		while(true) {
+			N++;
+			int S = 0;
+			int B = 0;
+			int O = 0;
+			String s = scan.nextLine();
+			for(int i=0; i<ran_str.length(); i++) {
+				if(ran_str.contains(String.valueOf(s.charAt(i)))) {
+					if(ran_str.charAt(i) == s.charAt(i)) {
+						S++;
+					}else {
+						B++;
+					}
+				}else {
+					O++;
+				}
+			}
+			System.out.println(N+" - S:"+S+", B:"+B+", O:"+O);
+			if(ran_str.equals(s)) {
+				System.out.println("(종료)");
+				break;
+			}
+		}
+		
 	}
 }
